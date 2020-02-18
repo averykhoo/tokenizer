9d5a3a65bad41573746956632533e26e49916f89
averykhoo <averykhoo@gmail.com>
2020-02-18 23:59:44 +0800

    more code

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,16 +1,43 @@
 import pickle
+from collections import Counter
 from dataclasses import dataclass, field
 from typing import Tuple, Dict, List
 
 
 @dataclass
 class BagOfWordsCorpus:
-    corpus: Dict[str, Tuple[Tuple[int, int], ...]] = field(default_factory=dict)
+    corpus: List[Tuple[Tuple[int, int], ...]] = field(default_factory=dict)
     vocabulary_lookup: Dict[str, int] = field(default_factory=dict)
     vocabulary_words: List[str] = field(default_factory=list)
 
-    def add(self, document):
-        pass
+    def add(self, document_words):
+        c = Counter(self.vocabulary(word) for word in document_words)
+        # todo: thread safe?
+        self.corpus.append(tuple(c.most_common()))
+        return len(self.corpus) - 1  # this is the document_idx
+
+    def words(self, document_idx):
+        for word_idx, count in self.corpus[document_idx]:
+            word = self.vocabulary_words[word_idx]
+            for _ in range(count):
+                yield word
+
+    def tf(self, document_idx):
+        return {self.vocabulary_lookup[word_idx]: count
+                for word_idx, count in self.corpus[document_idx]}
+
+    def idf(self, document_idxs):
+        c = Counter()
+        for document_idx in document_idxs:
+            c.update(self.unique_words(document_idx))
+        return c
+
+    def unique_words(self, document_idx):
+        for word_idx, count in self.corpus[document_idx]:
+            yield self.vocabulary_words[word_idx]
+
+    def len(self, document_idx):
+        return sum(count for _, count in self.corpus[document_idx])
 
     def vocabulary(self, item):
         # todo: make this work
