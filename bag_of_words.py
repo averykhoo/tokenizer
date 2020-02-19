010540e0dcaaa7236d3d98a805f93eb1f39c2bc0
Avery Khoo <averykhoo@gmail.com>
2020-02-19 15:53:18 +0800

    update bows

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,58 +1,93 @@
 import pickle
 from collections import Counter
-from dataclasses import dataclass, field
-from typing import Tuple, Dict, List
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Any
+from typing import Dict
+from typing import Generator
+from typing import Iterable
+from typing import List
+from typing import Tuple
+
+from tokenizer import unicode_tokenize
 
 
 @dataclass
 class BagOfWordsCorpus:
-    corpus: List[Tuple[Tuple[int, int], ...]] = field(default_factory=dict)
-    vocabulary_lookup: Dict[str, int] = field(default_factory=dict)
-    vocabulary_words: List[str] = field(default_factory=list)
+    # actual documents bags-of-words stored here, as tuples of (word_idx, word_count), sorted by word_count descending
+    corpus: List[Tuple[Tuple[int, int], ...]] = field(default_factory=list)
 
-    def add(self, document_words):
-        c = Counter(self.vocabulary(word) for word in document_words)
-        # todo: thread safe?
+    # vocabulary: word <-> word_index
+    _vocabulary_to_idx: Dict[str, int] = field(default_factory=dict)
+    _vocabulary: List[str] = field(default_factory=list)
+
+    def word_to_index(self, word: str) -> int:
+        # not thread safe!
+
+        # known word
+        if word in self._vocabulary_to_idx:
+            return self._vocabulary_to_idx[word]
+
+        # add word
+        assert isinstance(word, str), word
+        _idx = self._vocabulary_to_idx[word] = len(self._vocabulary)
+        self._vocabulary.append(word)
+
+        # double-check invariants before returning
+        assert len(self._vocabulary_to_idx) == len(self._vocabulary) == _idx + 1
+        assert self._vocabulary[_idx] == word, (self._vocabulary[_idx], word)  # check race condition
+        return _idx
+
+    def index_to_word(self, word_index: int) -> str:
+        return self._vocabulary[word_index]
+
+    def add_document(self, document_words: Iterable[str]) -> int:
+        # not thread safe!
+        c = Counter(self.word_to_index(word) for word in document_words)
         self.corpus.append(tuple(c.most_common()))
-        return len(self.corpus) - 1  # this is the document_idx
+        return len(self.corpus) - 1  # this is the document index
 
-    def words(self, document_idx):
-        for word_idx, count in self.corpus[document_idx]:
-            word = self.vocabulary_words[word_idx]
+    def add_document_text(self, document: str) -> int:
+        # not thread safe!
+        c = Counter(self.word_to_index(word) for word in unicode_tokenize(document, words_only=True))
+        self.corpus.append(tuple(c.most_common()))
+        return len(self.corpus) - 1  # this is the document index
+
+    def words(self, document_index: int) -> Generator[str, Any, None]:
+        for word_idx, count in self.corpus[document_index]:
+            word = self._vocabulary[word_idx]
             for _ in range(count):
                 yield word
 
-    def tf(self, document_idx):
-        return {self.vocabulary_lookup[word_idx]: count
-                for word_idx, count in self.corpus[document_idx]}
+    def tf(self, document_index: int) -> Dict[str, int]:
+        # todo: smoothing like TfidfVectorizer
+        return {self._vocabulary[word_idx]: count for word_idx, count in self.corpus[document_index]}
 
-    def idf(self, document_idxs):
-        c = Counter()
-        for document_idx in document_idxs:
-            c.update(self.unique_words(document_idx))
-        return c
+    def idf(self, document_indices: Iterable[int]) -> Dict[str, int]:
+        # todo: smoothing like TfidfVectorizer
+        _idx_idf = Counter()
+        for document_idx in document_indices:
+            _idx_idf.update(word_idx for word_idx, _ in self.corpus[document_idx])
+        return {self._vocabulary[word_idx]: count for word_idx, count in _idx_idf}
 
-    def unique_words(self, document_idx):
-        for word_idx, count in self.corpus[document_idx]:
-            yield self.vocabulary_words[word_idx]
+    def unique_words(self, document_index: int) -> Generator[str, Any, None]:
+        for word_idx, count in self.corpus[document_index]:
+            yield self._vocabulary[word_idx]
 
-    def len(self, document_idx):
-        return sum(count for _, count in self.corpus[document_idx])
-
-    def vocabulary(self, item):
-        # todo: make this work
-        if isinstance(item, str):
-            return self.vocabulary_lookup[item]
-        elif isinstance(item, int):
-            return self.vocabulary_words[item]
+    def num_words(self, document_index: int) -> int:
+        return sum(count for _, count in self.corpus[document_index])
 
     def to_pickle(self, path, protocol_level=2):
+        # not necessary to store `_vocabulary_to_idx` as it's just a reverse lookup table for `_vocabulary`
         with open(path, 'wb') as f:
-            pickle.dump((self.corpus, self.vocabulary_lookup, self.vocabulary_words), f, protocol=protocol_level)
+            pickle.dump((self.corpus, self._vocabulary), f, protocol=protocol_level)
             return f.tell()
 
     @staticmethod
     def from_pickle(path):
         with open(path, 'rb') as f:
-            corpus, vocab_lookup, vocab_words = pickle.load(f)
-            return BagOfWordsCorpus(corpus=corpus, vocabulary_lookup=vocab_lookup, vocabulary_words=vocab_words)
+            corpus, vocabulary = pickle.load(f)
+
+            return BagOfWordsCorpus(corpus=corpus,
+                                    _vocabulary_to_idx={word: idx for idx, word in enumerate(vocabulary)},
+                                    _vocabulary=vocabulary)
