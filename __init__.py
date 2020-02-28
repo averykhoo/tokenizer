04b72daae370e27e845627937be5ef7323b04dd7
Avery Khoo <averykhoo@gmail.com>
2020-02-28 16:00:51 +0800

    add whitespace

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
+from .tokenizer import Token
+from .tokenizer import TokenCategory
+
+from .tokenizer import unicode_tokenize
+from .tokenizer import word_n_grams
+
+from .tokenizer import sentence_split
+from .tokenizer import sentence_split_tokens
+
 from .tokenizer import is_punctuation_char
 from .tokenizer import is_space_char
 from .tokenizer import is_text_char
-from .tokenizer import sentence_split
-from .tokenizer import sentence_split_tokens
-from .tokenizer import Token
-from .tokenizer import TokenCategory
-from .tokenizer import unicode_tokenize
-from .tokenizer import word_n_grams
+
 from .tokenizer import CLOSING_PUNCTUATION
 from .tokenizer import UNICODE_SPACES
 from .tokenizer import UNPRINTABLE_CHARS
